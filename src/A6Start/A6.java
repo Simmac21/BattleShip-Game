1524681221083a4b42a6073947adec8800aff74f
Simmac21 <simranmacwan2198@gmail.com>
2022-06-14 19:59:11 -0400

    initial commit

---
+++ src/A6Start/A6.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A6Start;
+
+/**
+ * Starting code for COMP10205 - Assignment#6
+ * @author mark.yendt
+ */
+
+public class A6 {
+	
+    
+    
+    
+    
+    
+    
+    
+	public static void main(String[] args) {
+		
+		SampleBot gameboard1 = new SampleBot();
+		
+	}
+
+}
